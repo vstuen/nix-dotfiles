4e7c91c4e4c206337b9508c8263aed24ffab1d9e
Vegard Stuen <vegard.stuen@wideroe.no>
2025-04-29 15:28:08 +0200

    chore(home/vs-worktop): updates aws ssm shell aliases

--- home/hosts/vs-worktop/default.nix
+++ home/hosts/vs-worktop/default.nix
@@ -11,8 +11,8 @@
 
   programs.fish.shellAliases = {
     aws-ssm-jump = "aws ssm start-session --target i-05e21e3029568146c --profile prod";
-    aws-ssm-mw-prod1a = "aws --profile prod ssm start-session --target i-0f427d706f4c5d1a3";
-    aws-ssm-mw-prod1b = "aws --profile prod ssm start-session --target i-0e4396906e415ac3c";
+    aws-ssm-mw-prod1a = "aws --profile prod ssm start-session --target i-0d78cacc4d504f5e7";
+    aws-ssm-mw-prod1b = "aws --profile prod ssm start-session --target i-06ed875e51781fc84";
     aws-ssm-mw-stage1a = "aws --profile prod ssm start-session --target i-08e12745863f8808d";
     aws-ssm-mw-stage1b = "aws --profile prod ssm start-session --target i-0afcde19138ed8b56";
     aws-ssm-mw-dev = "aws ssm start-session --target i-06a06589be878490b";
