bd5417098db37e9769e8bb83c784ca79937586dc
Vegard Stuen <vegard.stuen@wideroe.no>
2025-08-17 13:22:41 +0200

    chore(nixos/common): set 1password gui to unstable package
    The application complained about mismatch in version and datastore
    version, so I needed the latest version available.

--- nixos/modules/common/default.nix
+++ nixos/modules/common/default.nix
@@ -186,8 +186,10 @@
   programs._1password.enable = true;
   programs._1password-gui = {
     enable = true;
+    package = pkgs-unstable._1password-gui;
     polkitPolicyOwners = [ username ];
   };
+  
   environment.etc = {
     "1password/custom_allowed_browsers" = {
       text = ''
